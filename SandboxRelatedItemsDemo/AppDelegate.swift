3acf28c84d2a6acec7763c152840215b894b9e09
freedom <stefano.vettor@mac.com>
2015-08-18 23:00:10 +0200

    Initial Commit

---
+++ SandboxRelatedItemsDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SandboxRelatedItemsDemo
+//
+//  Created by Stefano Vettor on 18/08/15.
+//  Copyright (c) 2015 Stefano Vettor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SandboxRelatedItemsDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SandboxRelatedItemsDemo
+//
+//  Created by Stefano Vettor on 18/08/15.
+//  Copyright (c) 2015 Stefano Vettor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
